37b4d65a9c15bbb752acca8450628d8cf78eaec8
TajAlDenAjek <taj.darkn10@gmail.com>
2023-06-15 13:57:46 +0300

    register && login validation

---
+++ middlewares/validation/index.js
@@ -0,0 +1,8 @@
+const path=require('path');
+const authValidation=require(path.join(__dirname,'authentication'));
+
+
+module.exports=
+{
+    authValidation,
+}
--- routes/authentication.js
+++ routes/authentication.js
@@ -2,9 +2,12 @@ const path=require('path');
 const express=require('express');
 const router=express.Router();
 const {authController}=require(path.join(__dirname,'..','controllers'));
+const {authValidation}=require(path.join(__dirname,'..','middlewares','validation'));
 
-router.post('/register',authController.register);
-router.post('/login',authController.login);
+
+
+router.post('/register',authValidation.register,authController.register);
+router.post('/login',authValidation.login,authController.login);
 
 module.exports = router;
 
