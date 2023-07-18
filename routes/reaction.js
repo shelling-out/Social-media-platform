83e48e11308a9913b81f39c5d0e7746dac51f74a
TajAlDenAjek <taj.darkn10@gmail.com>
2023-07-18 14:01:51 +0300

    create reaction controller

--- routes/reaction.js
+++ routes/reaction.js
@@ -2,10 +2,15 @@ const path=require('path');
 const express=require('express');
 const router=express.Router();
 const {reactionController}=require(path.join(__dirname,'..','controllers'));
-const {userValidation,postValidation}=require(path.join(__dirname,'..','middlewares','validation'));
+const {userValidation,postValidation,reactionValidation}=require(path.join(__dirname,'..','middlewares','validation'));
 
 
-router.post('/add/:id',postValidation.checkIdPostExestence,reactionController.createReaction);
+router.post('/add/:id',
+    postValidation.checkIdPostExestence,
+    reactionValidation.reactOnceOnly,
+    reactionValidation.reactionData,
+    reactionController.createReaction
+);
 
 
 
