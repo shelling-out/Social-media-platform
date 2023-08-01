dc463c9cabd48e74f84d3a6ba7356f596032e1fb
TajAlDenAjek <taj.darkn10@gmail.com>
2023-08-01 14:38:06 +0300

    comments authorizations

--- middlewares/authorization/index.js
+++ middlewares/authorization/index.js
@@ -1,7 +1,9 @@
 const path=require('path');
 const postAuth=require(path.join(__dirname,'post'));
+const commentAuth=require(path.join(__dirname,'comment'));
 
 
 module.exports={
-    postAuth
+    postAuth,
+    commentAuth
 }
