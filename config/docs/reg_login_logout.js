72da4e78045e244662eb4b3b4a8f5d06ed53c457
shelling-out <rami-zidan2002@hotmail.com>
2023-08-14 01:14:18 +0300

    modifying doc for register

--- config/docs/reg_login_logout.js
+++ config/docs/reg_login_logout.js
@@ -9,21 +9,44 @@ const register={
     </ul>
     <h3> Note : users are distinct by email </h3>
     `,
-    requestBody:{  
-        required:true,
-        content:{
-            "application/json":{
-                schema:{
-                    type:"Object",
-                    example:{
-                        username:"potato",
-                        email:"potato@email.com",
-                        password:"potatoPassword"
+    parameters:[
+        {
+          "name": "id",
+          "in": "query",
+          "type": "integer",
+          "required": true
+        },
+        {
+            in: 'formData',
+            name: 'data',
+            type: 'application/json',
+            description: 'JSON data to update make sure to give it the field name (data)',
+            content:{
+                "application/json":{
+                    schema:{
+                        type:"Object",
+                        example:{
+                            
+                            "username": "potatoUserName",
+                            "password": "potatoNewPassword",
+                            "firstName": "potatoFirstName",
+                            "lastName": "potatoLastName",
+                            "gender": "male",
+                            "birthday": "2022-01-01",
+                            "country": "potatoContury"       
+                        }
                     }
                 }
             }
         },
-    },
+        {
+            in: 'formData',
+            name: 'image',
+            type: 'file',
+            description: 'Image to update make sure to give it the field name (image)',
+            example: '{"image": "the picture you will upload"}',
+        },
+    ],
     responses:{
         201:{
             description:"created",
@@ -51,13 +74,19 @@ const register={
                               "email": [
                                 "The email field is required.",
                                 "Email is already in use !", 
-                                "The email format is invalid."
+                                "The email format is invalid."  
                               ],
                               "password": [
                                 "The password field is required.",
                                 "The password must be at least 8 characters.",
                                 "The password may not be greater than 20 characters."
-                            ]
+                            ],
+                            "gender": [
+                                "The selected gender is invalid."
+                            ],
+                            "birthday": [
+                                "The birthday is not a valid date format."
+                            ],
                         }
                     }
                 }
