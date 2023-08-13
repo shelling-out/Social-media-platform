37c6b3fa113f6b7607d051083dfeea20c308ac19
shelling-out <rami-zidan2002@hotmail.com>
2023-08-14 00:45:14 +0300

    modifying register controller to allow passing extra information about user

--- routes/authentication.js
+++ routes/authentication.js
@@ -3,11 +3,11 @@ const express=require('express');
 const router=express.Router();
 const {authController}=require(path.join(__dirname,'..','controllers'));
 const {authValidation}=require(path.join(__dirname,'..','middlewares','validation'));
-
+const uploadImage=require(path.join(__dirname,'..','middlewares','uploadImage'));
 const authenticated=require(path.join(__dirname,'..','middlewares','authentication.js'));
 
 
-router.post('/register',authValidation.register,authController.register);
+router.post('/register',uploadImage,authValidation.register,authController.register);
 router.post('/login',authValidation.login,authController.login);
 router.get('/logout',authenticated,authController.logout);
 
